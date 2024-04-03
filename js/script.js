87818a13b6762347ef5c0fce91fa94413dc2c282
EllisOllier <ellisollier1@gmail.com>
2024-04-03 14:14:29 +0100

    Added password match validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-document.querySelector('#create-account').addEventListener("click", function(){
-    
+const btnCreateAccount = document.getElementById('create-account');
+const passwordField = document.getElementById('password');
+const confirmPasswordField = document.getElementById('confirm-password');
+const passwordMatchMessage = document.getElementById('match-password');
+
+btnCreateAccount.addEventListener('click', function(e){
+    if(confirmPasswordField.value != passwordField.value || passwordField.value == ""){
+        e.preventDefault();
+        passwordField.style.border = "1px solid red";
+        confirmPasswordField.style.border = "1px solid red";
+        passwordMatchMessage.hidden = false;
+    } else {
+        console.log("Continue")
+    }
 })
